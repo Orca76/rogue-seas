d5225d4c15ff0c197f663de0f704fff0287ee826
Orca76 <orca67195@gmail.com>
2025-04-11 23:22:01 +0900

    walk
    プレイヤー移動処理

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float moveSpeed = 5f; // インスペクタから調整できる速度
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 入力取得
+        movement.x = Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
+        movement.y = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // 移動
+        rb.velocity = movement * moveSpeed;
+    }
+}
