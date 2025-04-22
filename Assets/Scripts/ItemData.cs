d264ab90f2192a5227fbce31b86474d747872718
Orca76 <orca67195@gmail.com>
2025-04-22 22:45:03 +0900

    itemdata

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string itemName;
+    public Sprite icon;
+    public GameObject itemPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
